8ca9c1bd664e625807e2b217c7a8943fce283cc9
jonas <jonas@DESKTOP-NDC3HRT>
2023-09-11 15:40:47 +0200

    Add project files.

---
+++ ElonMusk/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElonMusk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Car 1
+            Console.WriteLine("\nCar 1");
+            Console.Write("Initial distance driven (meters): ");
+            int car1DistanceDriven = int.Parse(Console.ReadLine());
+            Console.Write("Color: ");
+            string car1Color = Console.ReadLine();
+
+            // Car 2
+            Console.WriteLine("\nCar 2");
+            Console.Write("Initial distance driven (meters): ");
+            int car2DistanceDriven = int.Parse(Console.ReadLine());
+            Console.Write("Color: ");
+            string car2Color = Console.ReadLine();
+
+            RCCar car1 = new RCCar(car1DistanceDriven, car1Color);
+            RCCar car2 = new RCCar(car2DistanceDriven, car2Color);
+
+            // Test driving the cars
+            car1.Drive(30); // Drive car1 for 30 meters
+            car2.Drive(50); // Drive car2 for 50 meters
+
+            // Recharge the cars
+            car1.Recharge();
+            car2.Recharge();
+
+            Console.ReadLine(); // Wait for user input before exiting
+        }
+    }
+}
---
+++ ElonMusk/RCCar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElonMusk
+{
+    #region Model
+    public class RCCar
+    {
+        private sbyte batteryCapacity;
+        private int distanceDriven;
+        private sbyte batteryLife;
+        private string color;
+
+        public sbyte BatteryCapacity
+        {
+            get { return batteryCapacity; }
+            set { batteryCapacity = value; }
+        }
+
+        public int DistanceDriven
+        {
+            get { return distanceDriven; }
+            private set { distanceDriven = value; }
+        }
+
+        public sbyte BatteryLife
+        {
+            get { return batteryLife; }
+            private set { batteryLife = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            private set { color = value; }
+        }
+
+        public RCCar(int initialDistanceDriven, string carColor)
+        {
+            BatteryCapacity = 100;
+            DistanceDriven = initialDistanceDriven;
+            BatteryLife = BatteryCapacity;
+            Color = carColor;
+        }
+
+        public void Drive(int meters)
+        {
+            if (BatteryLife > 0)
+            {
+                int metersDriven = Math.Min(meters, BatteryLife * 20); // Ensure we don't drive more than the available battery allows
+                DistanceDriven += metersDriven;
+                int batteryDrain = metersDriven / 20; // Calculate the battery drain
+                BatteryLife -= (sbyte)batteryDrain;
+                UpdateDisplay();
+            }
+            else
+            {
+                Console.WriteLine("The car is out of battery and cannot drive.");
+                Console.ReadLine();
+
+            }
+        }
+
+        public void Recharge()
+        {
+            DistanceDriven = 0;
+            BatteryLife = BatteryCapacity;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            Console.WriteLine($"\nCar Color: {Color}");
+            Console.WriteLine($"Distance Driven: {DistanceDriven} meters");
+            Console.WriteLine($"Battery Life: {BatteryLife}%");
+            Console.ReadLine();
+        }
+    }
+    #endregion
+}
